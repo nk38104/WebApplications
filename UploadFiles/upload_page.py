bfda6a02edb89565e19d9f681f8503389144c655
Nikola Kelava <nikola.kelava94@gmail.com>
2020-11-23 19:38:47 +0100

    added basic upload page structure and functionality

--- UploadFiles/upload_page.py
+++ UploadFiles/upload_page.py
@@ -1,25 +1,90 @@
 #! C:\Users\Nikola Kelava\AppData\Local\Programs\Python\Python38-32\python.exe
 from cgi import FieldStorage
-from os import environ
+import os
 
 
-request_type = environ.get('REQUEST_METHOD', '')
+request_type = os.environ['REQUEST_METHOD']
+params = FieldStorage()
+img_dir_path = '../../../../htdocs/images'
 
-form = FieldStorage()
+if not os.path.isdir(img_dir_path): #inace ce vratiti gresku svaki put kad se post-a
+    os.mkdir(img_dir_path)
+ 
+images = os.listdir(img_dir_path)
 
-if(environ['REQUEST_METHOD'].upper() == 'POST'):
-    print('Location: login_page.py')
+if (request_type.upper() == 'POST'):
+    file_item = params["image"]
+
+    if file_item.filename:
+        img_dir_path = '../../../../htdocs/images/'
+        img_dir_path += os.path.basename(file_item.filename)
+        open(img_dir_path, 'wb').write(file_item.file.read(250000))
+        message = f'The file "{img_dir_path}" is uploaded successfully.'
+        success = True
+    else:
+        message = 'No file was uploaded'
+        success = False
+    
+    # if (success):
+    #     print('Location: upload_page.py')
+
 
 print('''
        <!DOCTYPE html>
         <html>
         <head>
+            <style>
+                body {
+                    margin: 0;
+                    padding: 0;
+                }
+                img {
+                    padding-right: 20px;
+                }
+                #image-container {
+                    margin: 60px 350px 0 350px;
+                }
+                form {
+                    margin-top: 120px;
+                    text-align: center;
+                }
+                h1 {
+                    text-align: center;
+                }
+                #logout {
+                    margin-right: 25px;
+                    margin-top: 10px;
+                    font-size: 20px;
+                    text-align: right;
+                }
+            </style>
         </head>
         <body>
-            <h1>Upload Page</h1>
+            <nav>
+                <div id="logout">
+                    <a href="login_page.py">Log out</a>
+                </div>
+            </nav>
             <div>
-                <a href="login_page.py">Log out</a> 
+                <h1>Upload Page</h1>
+            </div>''')
+
+print('''
+        <form enctype="multipart/form-data" method="POST">
+            <div id="upload_container">
+                <input type="file" name="image" accept="image/png, image/jpeg">
+                <input type="submit" name="upload" value="Upload">
             </div>
+        </form>
+        <div id="image-container">''')
+
+for image in images:
+    print(f'<img src="../../../../images/{image}" width=300 height=200 alt="{image}">')
+    
+if (request_type.upper() == 'POST' and (not success)):
+    print(f'<script>alert("Image upload failed!\\n{message}");</script>')
+
+print('''</div>
         </body>
         </html>''')
 
